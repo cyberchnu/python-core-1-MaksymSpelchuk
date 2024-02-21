f17ad8669e4e947b3f1766c44cb8d2961bd86fb9
spelc <spelciucmaxim@gmail.com>
2024-02-21 18:42:51 +0200

    Task 2 done

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,11 +1,11 @@
 def fizz_buzz(param):
   #Type your code here
+  if param % 3 == 0 and param % 5 == 0:
+      return "FizzBuzz"
   if param % 3 == 0:
       return "Fizz"
   if param % 5 == 0:
       return "Buzz"
-  if param % 3 == 0 and param % 5 == 0:
-      return "FizzBuzz"
   if param % 3 != 0 and param % 5 != 0:
       return str(param)
 
