be5263f4c890ad0edf879ef4fcb5d5c40be002b9
spelc <spelciucmaxim@gmail.com>
2024-02-21 19:45:54 +0200

    Task 6 done

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,9 +1,9 @@
 def int_within_bounds(number, lower_bound, upper_bound):
   # Type your code
+    if number % 1 != 0:
+      return False
     if number >= lower_bound and number < upper_bound:
       return True
-    if not isinstance(number, int):
-      return False
     else:
       return False
 
